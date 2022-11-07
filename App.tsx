28e926b702e7fc72c801eb50c2cafb3dd415b276
tbdev <tbdev@debian>
2022-11-07 16:24:22 -0300

    chore: multi deployment config

--- App.tsx
+++ App.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, Alert} from 'react-native';
 import Repos from './src/components/Repos';
 import codePush from 'react-native-code-push';
 
 let codePushOptions = {
   checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
-  updateDialog: {appendReleaseDescription: true},
+  installMode: codePush.InstallMode.ON_NEXT_RESTART,
+  // updateDialog: {appendReleaseDescription: true},
 };
 
 const App = () => {
+  useEffect(() => {
+    codePush.checkForUpdate().then(update => {
+      if (!update) {
+        Alert.alert('The app is up to date!');
+        console.log('The app is up to date!');
+      } else {
+        Alert.alert(
+          'An update is available! Should we download it?',
+          update.appVersion,
+        );
+        console.log('An update is available! Should we download it?');
+      }
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Olá Felipe</Text>
+      <View style={styles.header}>
+        <Text style={styles.text}>Lista de Repos</Text>
+      </View>
       <Repos />
     </View>
   );
@@ -24,11 +42,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
   },
+  header: {
+    backgroundColor: '#666',
+  },
   text: {
-    color: '#000',
+    color: '#63fa43',
     fontSize: 30,
     fontWeight: 'bold',
     padding: 20,
+    marginBottom: 30,
   },
 });
 
